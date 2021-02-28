7ccfa32345aebde757f8582c866d6bd730df546a
melondulzon <melondulzon@gmail.com>
2021-02-28 19:30:40 +0100

    api

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,4 @@
 const googleDocURL = 'https://docs.google.com/spreadsheets/d/11sh49qNBdo5qX59tahRapJOMBSWbV5wUfj41fwvWK8s/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-const googleApiKey = 'AIzaSyDBothgf6z9sol9L89EZhGXojdCQIu-u8M';
+const googleApiKey = process.env.API;
