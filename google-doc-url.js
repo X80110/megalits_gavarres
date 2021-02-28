4f4cc9cd0a41fce6971dad2777cd179aa420f5c6
melondulzon <melondulzon@gmail.com>
2021-02-28 19:29:24 +0100

    api changed account

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-const googleDocURL = 'https://docs.google.com/spreadsheets/d/1Rrh6RR9WYCzp7R3_ZSvWOFK1mARYhLokK6t8zVz8MEs/edit#gid=0';
+const googleDocURL = 'https://docs.google.com/spreadsheets/d/11sh49qNBdo5qX59tahRapJOMBSWbV5wUfj41fwvWK8s/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-const googleApiKey = process.env.API;
+const googleApiKey = 'AIzaSyDBothgf6z9sol9L89EZhGXojdCQIu-u8M';
